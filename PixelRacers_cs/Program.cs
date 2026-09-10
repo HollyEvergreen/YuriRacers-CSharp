dedc56a0891749d44a5430c9500db741cd2bec77
Holly <cam33328@gmail.com>
2026-09-10 02:22:31 +0100

    .

---
+++ PixelRacers_cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using Silk.NET.OpenGL;
+using Yui;
+new PixelRacers(true).run();
+
+internal class PixelRacers(bool debug = false) : App(debug)
+{
+	protected override void Start()
+	{
+		Log("hello\n");
+	}
+	protected override void OnLoad() { }
+	protected override void Update(double dt) { }
+	protected override void Render(double dt) { }
+}
+
